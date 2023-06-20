38a9813c057915233222935a7fc9f8538eb59335
dbroemme <darren.broemmer@gmail.com>
2023-06-19 22:43:04 -0400

    Use refactored code

--- news.py
+++ news.py
@@ -1,53 +1,41 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
-# Read the CSV file
-df = pd.read_csv('./data/news/news.csv', header=0)
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv("news.csv", header=0)
 
-# Convert the label column to a categorical variable
-df['label'] = df['label'].astype('category')
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.25)
-
-# Create a TF-IDF vectorizer
+# Create a TfidfVectorizer object
 vectorizer = TfidfVectorizer()
 
-# Transform the training and test data
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
+# Transform the text column into a sparse matrix of term frequencies
+X = vectorizer.fit_transform(df["text"])
 
-# Create a logistic regression model
+# Create a LogisticRegression model
 model = LogisticRegression()
 
-# Train the model on the training data
-model.fit(X_train_tfidf, y_train)
+# Train the model on the features and labels
+model.fit(X, df["label"])
 
-# Predict the labels for the test data
-y_pred = model.predict(X_test_tfidf)
+print("Accuracy: " + str(model.score(X, df['label'])))
 
-# Evaluate the model
-print("Model score: " + str(model.score(X_test_tfidf, y_test)))
+# Define a function to classify news
+def classify_news(text):
+    # Convert the text to a sparse matrix of term frequencies
+    X_new = vectorizer.transform([text])
 
-# Create a function to classify text as fake or real
-def classify_text(text):
-    # Transform the text data
-    text_tfidf = vectorizer.transform([text])
+    # Make a prediction using the trained model
+    prediction = model.predict(X_new)[0]
 
-    # Get the predicted label for the text data
-    label = model.predict(text_tfidf)[0]
-
-    # Return the label
-    return label
+    # Return the prediction
+    return prediction
 
 
 # Open the text file in read mode
 with open("article_the_onion.txt", "r") as f:
     article_text = f.read()
-    print("The Onion article: " + classify_text(article_text))
+    print("The Onion article: " + classify_news(article_text))
 
 with open("article_usa_today.txt", "r") as f:
     article_text = f.read()
-    print("USA Today article: " + classify_text(article_text))
+    print("USA Today article: " + classify_news(article_text))
